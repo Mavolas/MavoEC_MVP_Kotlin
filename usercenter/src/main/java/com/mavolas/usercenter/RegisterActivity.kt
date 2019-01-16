eff63d667fe8ccf370a273f774edf1a4c6009a62
Mavolas <sgngqian@sina.com>
2019-01-16 20:31:34 +0800

    框架配置正确执行

--- usercenter/src/main/java/com/mavolas/usercenter/RegisterActivity.kt
+++ usercenter/src/main/java/com/mavolas/usercenter/RegisterActivity.kt
@@ -1,13 +1,9 @@
 package com.mavolas.usercenter
 
 import android.os.Bundle
-import android.widget.Toast
 import com.mavolas.base.ui.activity.BaseMvpActivity
-import com.mavolas.usercenter.R.id.mRegisterBtn
 import com.mavolas.usercenter.presenter.RegisterPresenter
 import com.mavolas.usercenter.view.RegisterView
-import kotlinx.android.synthetic.main.activity_register.*
-
 
 
 /**
@@ -23,13 +19,8 @@ class RegisterActivity : BaseMvpActivity<RegisterPresenter>(),RegisterView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
         mPresenter = RegisterPresenter();
         mPresenter.mView = this;
-
-//        mRegisterBtn.setOnClickListener{
-//            mPresenter.register("","","")
-//        }
     }
 
 
